06fb142de3b6aa56420a6120dec06d4e20265eb6
L_L_Z <1284825730@qq.com>
2022-10-12 21:43:21 +0800

    项目初始化

---
+++ rollup.config.js
@@ -0,0 +1,1 @@
+export default { input: "src/index.js", output: { file: "dist/bundle.js", format: "cjs" } };
---
+++ src/index.js
@@ -0,0 +1,6 @@
+// import { sum } from "lodash-es";
+
+export default function () {
+	console.log('hello');
+	// console.log(sum[(1, 2)]);
+}
